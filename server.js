0f9486727cd8a184311b446cf5c916d830195753
Matthew Raycroft <mattraycroft@nc.rr.com>
2020-10-04 18:36:08 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -4,29 +4,14 @@ const port = 3000;
 
 app.use('/', express.static('public'));
 
-const budget = {
-    myBudget:[
-    {
-        title:'Eat out',
-        budget: 25
-    },
-    {
-        title: 'Rent',
-        budget: 375
-    },
-    {
-        title : 'Grocery',
-        budget: 110
-    },
-]};
-
-
 app.get('/hello', (req, res) =>{
     res.send('Hello World!');
 });
 
+
 app.get(`/budget`, (req, res) =>{
-    res.json(budget);
+    var json = require('./source.json')
+    res.json(json.myBudget);
 });
 
 app.listen(port, () =>{
