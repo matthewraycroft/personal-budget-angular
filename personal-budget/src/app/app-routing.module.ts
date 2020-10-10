a65cfb51ebeba92ca6c803c82d7654dc2ecefffb
Matthew Raycroft <mattraycroft@nc.rr.com>
2020-10-09 21:22:02 -0400

    Angular and Contact

--- personal-budget/src/app/app-routing.module.ts
+++ personal-budget/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
 import { P404Component } from './p404/p404.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 const routes: Routes = [
   {
@@ -18,6 +19,10 @@ const routes: Routes = [
   {
     path : 'login',
     component: LoginComponent
+  },
+  {
+    path : 'contact',
+    component: ContactsComponent
   },
   {
     path : '**',
